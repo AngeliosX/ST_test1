f06ee13f866183783c75db341e20b370dc0814b3
Angelios <angelionus@gmail.com>
2022-06-08 20:43:37 +0500

    Spring Boot + Bootstrap

---
+++ src/main/java/com/example/ST_test1/StTest1Application.java
@@ -0,0 +1,13 @@
+package com.example.ST_test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StTest1Application.class, args);
+	}
+
+}
